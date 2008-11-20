4f9b977b213d31611f722f523e6f21b41d4a2968
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-11-19 22:00:57 -0800

    Updated gem dependencies
    * Temporarily commented out merb and dm gems that are not being used yet

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -8,16 +8,16 @@ dependency "system_timer", "1.0"
 dependency "webrat",       "0.3.2"
 
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0"
-dm_gems_version   = "0.9.6"
+merb_gems_version = "1.0.1"
+dm_gems_version   = "0.9.7"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version
-dependency "merb-cache", merb_gems_version
+#dependency "merb-cache", merb_gems_version
 dependency "merb-haml", merb_gems_version
 dependency "merb-helpers", merb_gems_version
-dependency "merb-mailer", merb_gems_version
+#dependency "merb-mailer", merb_gems_version
 dependency "merb-slices", merb_gems_version
 dependency "merb-auth-core", merb_gems_version
 dependency "merb-auth-more", merb_gems_version
@@ -26,9 +26,9 @@ dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
 
 dependency "dm-core", dm_gems_version
-dependency "dm-aggregates", dm_gems_version
+#dependency "dm-aggregates", dm_gems_version
 dependency "dm-constraints", dm_gems_version
-dependency "dm-migrations", dm_gems_version
-dependency "dm-timestamps", dm_gems_version
-dependency "dm-types", dm_gems_version
+#dependency "dm-migrations", dm_gems_version
+#dependency "dm-timestamps", dm_gems_version
+#dependency "dm-types", dm_gems_version
 dependency "dm-validations", dm_gems_version
