b737a8b5a800ad62d37f2f580af655f5d5840add
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-12-12 15:28:09 -0800

    Updated gem dependencies
    * Added gem install instructions for compass using github as a source
      until it is released on rubyforge

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,11 +1,11 @@
-dependency "compass",      "0.1.0"  # From: http://github.com/chriseppstein/compass/
-dependency "haml",         "2.1.0"  # From: http://github.com/nex3/haml/
-dependency "ParseTree",    "3.0.2", :require_as => "parse_tree"
-dependency "ruby2ruby",    "1.2.1"
-dependency "ruby-openid",  "2.1.2", :require_as => "openid"
-dependency "rack-cache",   "0.2.0", :require_as => "rack/cache"
-dependency "system_timer", "1.0"
-dependency "webrat",       "0.3.2"
+dependency "chriseppstein-compass", "0.3.4", :require_as => "compass"     # sudo gem install chriseppstein-compass --source http://gems.github.com
+dependency "haml",                  "2.1.0"                               # From: http://github.com/nex3/haml/
+dependency "ParseTree",             "3.0.2", :require_as => "parse_tree"
+dependency "ruby2ruby",             "1.2.1"
+dependency "ruby-openid",           "2.1.2", :require_as => "openid"
+dependency "rack-cache",            "0.2.0", :require_as => "rack/cache"
+dependency "system_timer",          "1.0"
+dependency "webrat",                "0.3.2"
 
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
 merb_gems_version = "1.0.4"
